bcbbfb1ef9912ae7fbb27a62a4038d45041d48b2
Dong Huang <huangdgm@gmail.com>
2017-11-29 10:30:14 +1300

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
     <div class="menuitem">Link 4</div>
   </div>
 
-  <div class="main">
+  <div class="main" width="600" height="400">
     <iframe src='https://webchat.botframework.com/embed/dongmybot?s=GJ14s70Rj8o.cwA.4Oo.vqDtk4rKypOUes-WbOIWraDVMH5eWw9Sy2-1_j47YXM'></iframe>
   </div>
 
